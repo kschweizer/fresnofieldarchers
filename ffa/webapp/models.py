d367e1e46e86c4fffee4edeea753237f36a7ddd6
Kyle Schweizer <kschweizer@berkeley.edu>
2021-08-31 17:12:14 -0700

    Updated Event model and serializer, updated API response when creating new event

--- ffa/webapp/models.py
+++ ffa/webapp/models.py
@@ -2,6 +2,8 @@ import calendar
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -15,7 +17,7 @@ class Blogpost(models.Model):
         return  "{} {}, {}".format(calendar.month_name[self.created_at.month], self.created_at.day, self.created_at.year)
 
 class Album(models.Model):
-    title = models.CharField(unique=True, max_length=200)
+    title = models.CharField(unique=True, max_length=200, blank=False)
     thumbnail = models.CharField(max_length=64, blank=True)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -53,7 +55,7 @@ class Image(models.Model):
 
         suf = SimpleUploadedFile(os.path.split(self.image.name)[-1], temp_handle.read(), content_type=CONTENT_TYPE)
         self.thumbnail.save('{}_thumbnail.{}'.format(os.path.splitext(suf.name)[0], FILE_EXTENSION), suf, save=False)
-    
+
     def save(self, *args, **kwargs):
         self.create_thumbnail()
         force_update = False
@@ -61,48 +63,37 @@ class Image(models.Model):
             force_update = True
         super(Image, self).save(force_update=force_update)
 
+# Delete ImageField on S3 storage
+@receiver(pre_delete, sender=Image)
+def image_delete_handler(sender, instance, **kwargs):
+    instance.image.delete(save=False)
+    instance.thumbnail.delete(save=False)
+
 class Event(models.Model):
     name = models.CharField(max_length=500, blank=False)
     description = models.TextField(blank=True)
-    flyer = models.ImageField(upload_to='events/flyers/', blank=True, null=True)
-    flyer_thumbnail = models.ImageField(upload_to='events/flyers/', blank=True, null=True)
+    flyer = models.FileField(upload_to='events/flyers/', blank=True, null=True)
     date = models.TextField(blank=True)
     scores = models.FileField(upload_to='events/scores/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def create_thumbnail(self):
-        if not self.flyer:
-            return
-        
-        import os
-        from PIL import Image as PIL_Image
-        from io import BytesIO
-        from django.core.files.uploadedfile import SimpleUploadedFile
-        
-        THUMB_SIZE = (250, 250)
+    @property
+    def format_date(self):
+        date = self.date
+        res = None
+        if (date):
+            start, end = str.split(date, ',')
+            start_tokens = str.split(start, ' ')
+            start = start_tokens[1] + ' ' + start_tokens[2] + ', ' + start_tokens[3]
+            if (end):
+                end_tokens = str.split(end, ' ')
+                end = end_tokens[1] + ' ' + end_tokens[2] + ', ' + end_tokens[3]
+                res = start + ' - ' + end
+            else:
+                res = start
+        return res
 
-        CONTENT_TYPE = self.flyer.file.content_type
-
-        if CONTENT_TYPE == 'image/jpeg':
-            PIL_TYPE = 'jpeg'
-            FILE_EXTENSION = 'jpg'
-        elif CONTENT_TYPE == 'image/png':
-            PIL_TYPE = 'png'
-            FILE_EXTENSION = 'png'
-
-        image = PIL_Image.open(BytesIO(self.flyer.read()))
-
-        image.thumbnail(THUMB_SIZE, PIL_Image.ANTIALIAS)
-
-        temp_handle = BytesIO()
-        image.save(temp_handle, PIL_TYPE)
-        temp_handle.seek(0)
-
-        suf = SimpleUploadedFile(os.path.split(self.flyer.name)[-1], temp_handle.read(), content_type=CONTENT_TYPE)
-        self.flyer_thumbnail.save('{}_thumbnail.{}'.format(os.path.splitext(suf.name)[0], FILE_EXTENSION), suf, save=False)
-
-    def save(self, *args, **kwargs):
-        self.create_thumbnail()
-        force_update = False
-        if self.id:
-            force_update = True
-        super(Event, self).save(force_update=force_update)
+# Delete ImageField on S3
+@receiver(pre_delete, sender=Event)
+def flyer_delete_handler(sender, instance, **kwargs):
+    instance.flyer.delete(save=False)
