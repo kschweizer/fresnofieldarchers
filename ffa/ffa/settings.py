00930fe08351882dfda0f2c2fdc5d5199b6de800
Kyle Schweizer <kschweizer@berkeley.edu>
2021-10-11 19:41:45 -0700

    settings patch

--- ffa/ffa/settings.py
+++ ffa/ffa/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = (os.getenv('DJANGO_DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = ['157.245.174.129', 'localhost']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
