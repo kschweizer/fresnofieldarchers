c114e5b13f56e0d9ca41e7e515a511ad8cb678be
Kyle Schweizer <kschweizer@berkeley.edu>
2021-11-30 13:07:55 -0800

    moved some url paths into respective apps instead of main ffa app

--- ffa/frontend/urls.py
+++ ffa/frontend/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('about-us', views.index),
+    path('events', views.index),
+    path('membership', views.index),
+    path('range', views.index),
+    path('photos', views.index),
+    path('login', views.index),
+    path('register', views.index),
+    path('logout', views.index),
+    path('editor', views.index),
+    re_path(r'editor/.*', views.index),
+    re_path(r'^photos/albums', views.index),
+    path('account', views.index)
 ]
--- ffa/webapp/urls.py
+++ ffa/webapp/urls.py
@@ -3,11 +3,11 @@ from rest_framework import routers
 from .api import AboutViewSet, BlogpostViewSet, ImageViewSet, AlbumViewSet, EventViewSet, PinnedEventViewSet
 
 router = routers.DefaultRouter()
-router.register('api/webapp/blogposts', BlogpostViewSet, '')
-router.register('api/webapp/photos', ImageViewSet, '')
-router.register('api/webapp/albums', AlbumViewSet, '')
-router.register('api/webapp/events', EventViewSet, '')
-router.register('api/webapp/about', AboutViewSet, '')
-router.register('api/webapp/pinnedevent', PinnedEventViewSet, '')
+router.register('webapp/blogposts', BlogpostViewSet, '')
+router.register('webapp/photos', ImageViewSet, '')
+router.register('webapp/albums', AlbumViewSet, '')
+router.register('webapp/events', EventViewSet, '')
+router.register('webapp/about', AboutViewSet, '')
+router.register('webapp/pinnedevent', PinnedEventViewSet, '')
 
 urlpatterns = router.urls
